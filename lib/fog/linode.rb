7d73b8454c34057c05349f88e20b4154306de4c3
Charles Treatman <ctreatman@linode.com>
2019-04-24 13:36:10 -0400

    Define empty Compute service

--- lib/fog/linode.rb
+++ lib/fog/linode.rb
@@ -1,7 +1,15 @@
-require "fog/linode/version"
+require 'fog/core'
+require 'fog/json'
+require 'fog/linode/version'
 
 module Fog
+  # Autoload Module for Compute
+  module Compute
+    autoload :Linode, File.expand_path('linode/compute', __dir__)
+  end
+
   module Linode
-    # Your code goes here...
+    extend Fog::Provider
+    service(:compute, 'Compute')
   end
 end
---
+++ lib/fog/linode/compute.rb
@@ -0,0 +1,6 @@
+module Fog
+  module Compute
+    class Linode < Fog::Service
+    end
+  end
+end
